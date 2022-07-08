0947f687944ea08b41c9a35b0911d079c914a1e0
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-08 08:05:56 -0300

    Adjusted to be more idiomatic

--- chap03/examples/ex01/account/account.go
+++ chap03/examples/ex01/account/account.go
@@ -1,17 +1,5 @@
 package account
 
 type Account struct {
-	name string
-}
-
-func (a *Account) SetName(name string) {
-
-	a.name = name
-
-}
-
-func (a *Account) GetName() string {
-
-	return a.name
-
+	Name string
 }
